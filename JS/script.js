47895dbec2483f2ac4e2437260182fcf68716091
andreaconv <andrea2convertini@gmail.com>
2023-03-29 15:23:46 +0200

    [created] ciclo per generare i box

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,20 @@
 //BOTTONI
 const btnGenera = document.getElementById("genera");
 
+//CONTAINER
+const container = document.querySelector(".container");
+
+//ciclo per generare i box all'interno del container
+for (let i = 0; i < 100; i++){
+  const box = document.createElement("div");
+  box.className = "box";
+  container.appendChild(box);
+}
+
+
 btnGenera.addEventListener("click", function(){
   console.log("mi hai cliccato");
 })
-
-//CONTAINER
-const container = document.querySelector(".container");
-container.innerHTML = `ciao`
-
-
 
 
 
