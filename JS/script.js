8959908e29a2a07c49b92a89d0330b331f1cd854
andreaconv <andrea2convertini@gmail.com>
2023-03-29 15:14:23 +0200

    verifica JS

---
+++ JS/script.js
@@ -0,0 +1,20 @@
+//BOTTONI
+const btnGenera = document.getElementById("genera");
+
+btnGenera.addEventListener("click", function(){
+  console.log("mi hai cliccato");
+})
+
+//CONTAINER
+const container = document.querySelector(".container");
+container.innerHTML = `ciao`
+
+
+
+
+
+
+
+
+
+
