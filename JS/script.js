257e3c6efce87993dee2cfda81b056f8af5b240f
andreaconv <andrea2convertini@gmail.com>
2023-03-29 17:51:31 +0200

    end version 2 con numeri random

--- JS/script.js
+++ JS/script.js
@@ -4,6 +4,9 @@ const btnGenera = document.getElementById("genera");
 //MAIN
 const main = document.getElementById("main");
 
+const blackList = [];
+
+
 //EVENTO bottone START cliccato
 btnGenera.addEventListener("click", function(){
   //creo il container
@@ -16,13 +19,18 @@ btnGenera.addEventListener("click", function(){
   const box = document.createElement("div");
   box.className = "box";
 
+  //creo un numero random
+  const numeroRandom = generateUniqueRandomNumber(blackList, 1, 100);
+  blackList.push(numeroRandom);
+  box.innerText = numeroRandom;
+
   //EVENTO click sul BOX
   box.addEventListener("click", function(){
   //coloro la casella aggiungendo una classe
   // FIXME:box.classList.add("active");
   //con la proprità toggle aggiunge o rimuove una classe
   box.classList.toggle("active")
-  console.log(box.innerText = i);
+  console.log(box.innerText)
 
   })
 
@@ -32,23 +40,40 @@ btnGenera.addEventListener("click", function(){
 
   //elimino il bottone
   btnGenera.className = "hide";
-})
 
+})
 
 
 
 
 //-------------------------FUNCTIONS-----------------------------------
 
-function createDiv (){
-  //credo sia meglio creare una funzione che crea il div e me lo inserisce nell'html dato che eseguo questa azione più di una volta;
-}
 
+/**
+ * genera un numero random non presente nella blacklist
+ * 
+ * @param {Array} arrayBlackList 
+ * @param {number} min 
+ * @param {number} max 
+ * @returns unique random generated
+ */
 
-
-
-
-
-
-
-
+function generateUniqueRandomNumber(arrBlackList, min, max){
+  /*
+  1. estraggo un numero random
+  2. se non è presente nella blacklist lo restituisco
+  3. se è presente ne genero un'altro fino a trovarne uno valido
+  */
+    let randomNumber;
+    let isValidNumber = false;
+  
+    while(!isValidNumber){
+      randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
+  
+      if(!arrBlackList.includes(randomNumber)){
+        //se il numero non è incluso nella blacklist isValidNumber diventa true e interrompe il ciclo
+        isValidNumber = true;
+      }
+    }
+    return randomNumber;
+  }
