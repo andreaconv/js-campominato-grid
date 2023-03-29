f8d3251859c22d5e050d4ab5eea206c0340188e0
andreaconv <andrea2convertini@gmail.com>
2023-03-29 15:46:50 +0200

    richiesta ticket

--- JS/script.js
+++ JS/script.js
@@ -4,7 +4,7 @@ const btnGenera = document.getElementById("genera");
 //MAIN
 const main = document.getElementById("main");
 
-//EVENTO bottone cliccato
+//EVENTO bottone START cliccato
 btnGenera.addEventListener("click", function(){
   //creo il container
   const container = document.createElement("div");
@@ -12,22 +12,30 @@ btnGenera.addEventListener("click", function(){
   main.appendChild(container);
 
   //ciclo per generare i box all'interno del container
-    for (let i = 0; i < 100; i++){
-    const box = document.createElement("div");
-    box.className = "box";
-    container.appendChild(box);
+  for (let i = 0; i < 100; i++){
+  const box = document.createElement("div");
+  box.className = "box";
+  container.appendChild(box);
   }
 
   //elimino il bottone
   btnGenera.className = "hide";
-
-
-  
-
 })
 
 
+//PROBLEMA: il box non viene visto al di fuori dell ciclo 
+//EVENTO click sulla casella
+box.addEventListener("click", function(){
+  //coloro la casella aggiungendo una classe
+  box.className = "active"
+})
 
+
+//-------------------------FUNCTIONS-----------------------------------
+
+function createDiv (){
+  //credo sia meglio creare una funzione che crea il div e me lo inserisce nell'html dato che eseguo questa azione più di una volta;
+}
 
 
 
